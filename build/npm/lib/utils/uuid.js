66562d653caee9dd08e4bf4e7c87f70050c02057
Shai Mohaban <shaimo@gmail.com>
2016-03-08 12:35:55 +0200

    Change uuid to use seed so it generates the same sequence across runs. This is needed for server-side rendering to be identical to client

--- build/npm/lib/utils/uuid.js
+++ build/npm/lib/utils/uuid.js
@@ -3,12 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+// Had to make uuid use a constant seed so it would generate same sequence across runs. This was needed
+// so server side rendering and client side rendering yield same results (e.g. the uuid is used when rendering
+// the concunctions with their name and id)
+var query_builder_seed = 0;
 
 exports.default = function () {
   return(
     // Generate a random GUID http://stackoverflow.com/a/2117523.
     'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      var r = Math.random() * 16 | 0;
+      //    const r = Math.random() * 16 | 0;
+      var r = query_builder_seed++ & 0xf;
       var v = c === 'x' ? r : r & 0x3 | 0x8;
       return v.toString(16);
     })
--- modules/utils/uuid.js
+++ modules/utils/uuid.js
@@ -1,7 +1,12 @@
+// Had to make uuid use a constant seed so it would generate same sequence across runs. This was needed
+// so server side rendering and client side rendering yield same results (e.g. the uuid is used when rendering
+// the concunctions with their name and id)
+var query_builder_seed = 0;
 export default () =>
   // Generate a random GUID http://stackoverflow.com/a/2117523.
   'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = Math.random() * 16 | 0;
+//    const r = Math.random() * 16 | 0;
+    const r = (query_builder_seed++) & 0xf;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
