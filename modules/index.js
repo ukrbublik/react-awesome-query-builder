49fa013be5f95be3bc9cb1d09ec40aaf05b5d76b
Vladimir Fedorenko (@mac) <vvf7723@gmail.com>
2016-04-06 17:15:08 +0300

    export functions for converting expression tree

--- modules/index.js
+++ modules/index.js
@@ -7,4 +7,4 @@ export { default as Query } from './components/Query';
 export { default as Builder } from './components/Builder';
 export { default as Preview } from './components/Preview';
 
-export { TextWidget, SelectWidget, DateWidget } from './components/widgets';
+export {queryBuilderFormat, queryBuilderToTree} from './utils/queryBuilderFormat';
