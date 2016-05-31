ecf1481b3c91a77d30b195553b9ef8b3054ab785
Vladimir Fedorenko (@mac) <vvf7723@gmail.com>
2016-05-31 14:59:03 +0300

    UUID util now create some part from math.random and from current time

--- build/npm/lib/utils/uuid.js
+++ build/npm/lib/utils/uuid.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
 // Had to make uuid use a constant seed so it would generate same sequence across runs. This was needed
 // so server side rendering and client side rendering yield same results (e.g. the uuid is used when rendering
@@ -9,13 +9,13 @@ Object.defineProperty(exports, "__esModule", {
 var query_builder_seed = 0;
 
 exports.default = function () {
-  return(
     // Generate a random GUID http://stackoverflow.com/a/2117523.
-    'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-      //    const r = Math.random() * 16 | 0;
-      var r = query_builder_seed++ & 0xf;
-      var v = c === 'x' ? r : r & 0x3 | 0x8;
-      return v.toString(16);
-    })
-  );
+    var timePart = (new Date().getTime().toString(16) + 'FF').substr(0, 11);
+    var s = 'ssssssss-xxxx-4xxx-yxxx-x'.replace(/[xys]/g, function (c) {
+        //    const r = ;
+        var r = c === 's' ? Math.floor(Math.random() * 16) : query_builder_seed++ & 0xf;
+        var v = c === 'x' ? r : r & 0x3 | 0x8;
+        return v.toString(16);
+    }) + timePart;
+    return s;
 };
