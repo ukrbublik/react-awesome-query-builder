4e737e0a100659b764fd21f2948bceb450dc636f
Denis Oblogin <ukrbublik@gmail.com>
2017-08-23 12:29:04 +0300

    update

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -6,7 +6,7 @@
 mkdir /tmp/react-awesome-query-builder -p
 rm -rf /tmp/react-awesome-query-builder/gh-pages
 mkdir /tmp/react-awesome-query-builder/gh-pages -p
-cp ./examples/bundle.js ./examples/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
+cp ./gh-pages/bundle.js ./gh-pages/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
 git checkout gh-pages
 cp /tmp/react-awesome-query-builder/gh-pages/* . -R
 git commit -m "update"
