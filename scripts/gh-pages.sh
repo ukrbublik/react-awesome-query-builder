ad9dfcce91b80a307f07eb15d9a52671d27ddcdf
Denis Oblogin <ukrbublik@gmail.com>
2017-08-23 12:33:47 +0300

    .

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,14 +1,14 @@
-#npm run build-global
+npm run build-global
 
-#npm run build-npm
+npm run build-npm
 
-#npm run build-examples
+npm run build-examples
 mkdir /tmp/react-awesome-query-builder -p
 rm -rf /tmp/react-awesome-query-builder/gh-pages
 mkdir /tmp/react-awesome-query-builder/gh-pages -p
-cp ./gh-pages/bundle.js ./gh-pages/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
+cp ./examples/bundle.js ./examples/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
 git checkout gh-pages
 cp /tmp/react-awesome-query-builder/gh-pages/* . -R
 git commit -m "update"
-#git checkout master
+git checkout master
 
