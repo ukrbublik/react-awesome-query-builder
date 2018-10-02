6599552177c49ebcdec4b294bf83028b6e0f48de
Evgeniy Tortumashev <mocart1567@gmail.com>
2018-10-02 12:38:48 +0700

    Added local states in Text and Number Widgets for buffering changes Replaced outdated text ref with functional setRef

--- modules/components/widgets/Number.js
+++ modules/components/widgets/Number.js
@@ -6,6 +6,21 @@ import 'antd/lib/date-picker/style';
 import {getFieldConfig} from '../../utils/configUtils';
 
 export default class NumberWidget extends Component {
+  constructor( props ) {
+    super( props );
+    this.timer = null;
+    this.setValueStorage = this.setValueStorage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      localValue:this.props.value,
+      lastSetValueTime:performance.now(),
+    }
+    this.numRef = null;
+    this.setNumberRef = element => {
+      this.numRef = element;
+    }
+  }
+
   static propTypes = {
     setValue: PropTypes.func.isRequired,
     min: PropTypes.number,
@@ -16,18 +31,57 @@ export default class NumberWidget extends Component {
     field: PropTypes.string.isRequired,
     value: PropTypes.number,
     customProps: PropTypes.object,
+    minTimeOutSetValue: PropTypes.number,
   };
 
+  // Set value at global state
+  setValueStorage(value){
+    this.props.setValue(value);
+    Promise.resolve(1)
+    .then(()=>{
+      this.setState({lastSetValueTime:performance.now()})
+      if(this.timer){
+        clearTimeout(this.timer);
+        this.timer = null;
+      }
+    });
+  }
+
   handleChange(val) {
+    const { minTimeOutSetValue } = this.props;
+    // Set value in local state
+    this.setState({
+      localValue:val
+    });
     if (val === '')
       val = undefined;
-    this.props.setValue(val);
+    const { lastSetValueTime } = this.state;
+    const nowTick = performance.now();
+    // If the change in value is not too fast,
+    // then save it in global state.
+    if(nowTick-lastSetValueTime>minTimeOutSetValue){
+      this.setValueStorage(val);
+    }else{
+      // If changes happen too fast,
+      // then save the value in the global state, with a delay,
+      // so that responsiveness does not suffer
+      if(!this.timer){
+        this.timer = setTimeout(
+          ()=>{
+            const { localValue:timeOutValue } = this.state;
+            this.setValueStorage(timeOutValue);
+          }, 
+          minTimeOutSetValue-(nowTick-lastSetValueTime)
+        );
+      }
+    }
   }
 
   static defaultProps = {
       min: undefined,
       max: undefined,
       step: undefined,
+    minTimeOutSetValue:1000
   };
 
   render() {
@@ -43,8 +97,8 @@ export default class NumberWidget extends Component {
         <InputNumber
           key="widget-number"
           size={this.props.config.settings.renderSize || "small"}
-          ref="num"
-          value={this.props.value != undefined ? this.props.value : null}
+          ref={ this.setNumberRef }
+          value={this.state.localValue != undefined ? this.state.localValue : null}
           min={min}
           max={max}
           step={step}
--- modules/components/widgets/Text.js
+++ modules/components/widgets/Text.js
@@ -4,6 +4,21 @@ import ReactDOM from 'react-dom';
 import { Input, Col } from 'antd';
 
 export default class TextWidget extends Component {
+  constructor(props) {
+    super(props);
+    this.timer = null;
+    this.setValueStorage = this.setValueStorage.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      localValue:this.props.value,
+      lastSetValueTime:performance.now()
+    }
+    this.textRef = null;
+    this.setTextRef = element => {
+      this.textRef = element;
+    }
+  }
+
   static propTypes = {
     setValue: PropTypes.func.isRequired,
     placeholder: PropTypes.string,
@@ -11,10 +26,53 @@ export default class TextWidget extends Component {
     value: PropTypes.string,
     field: PropTypes.string.isRequired,
     customProps: PropTypes.object,
+    minTimeOutSetValue: PropTypes.number,
   };
 
-  handleChange() {
-    this.props.setValue(ReactDOM.findDOMNode(this.refs.text).value);
+  static defaultProps = {
+    minTimeOutSetValue:1000
+  };
+  // Set value at global state
+  setValueStorage(value){
+    this.props.setValue(value);
+    Promise.resolve(1)
+    .then(()=>{
+      this.setState({lastSetValueTime:performance.now()})
+      if(this.timer){
+        clearTimeout(this.timer);
+        this.timer = null;
+      }
+    });
+  }
+
+
+  handleChange(val) {
+    const { value } = ReactDOM.findDOMNode(this.textRef);
+    const { minTimeOutSetValue } = this.props;
+    // Set value in local state
+    this.setState({
+      localValue:value
+    });
+    const { lastSetValueTime } = this.state;
+    const nowTick = performance.now();
+    // If the change in value is not too fast,
+    // then save it in global state.
+    if(nowTick-lastSetValueTime>minTimeOutSetValue){
+      this.setValueStorage(value);
+    }else{
+      // If changes happen too fast,
+      // then save the value in the global state, with a delay,
+      // so that responsiveness does not suffer
+      if(!this.timer){
+        this.timer = setTimeout(
+          ()=>{
+            const { localValue:timeOutValue } = this.state;
+            this.setValueStorage(timeOutValue);
+          }, 
+          minTimeOutSetValue-(nowTick-lastSetValueTime)
+        );
+      }
+    }
   }
 
   render() {
@@ -25,11 +83,11 @@ export default class TextWidget extends Component {
         <Input
           key="widget-text"
           size={this.props.config.settings.renderSize || "small"}
-          ref="text"
+          ref={ this.setTextRef }
           type={"text"}
-          value={this.props.value || null}
+          value={this.state.localValue || null}
           placeholder={this.props.placeholder}
-          onChange={this.handleChange.bind(this)}
+          onChange={this.handleChange}
           {...customProps}
         />
       </Col>
