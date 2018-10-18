b1c4b10fca54bf35619859f9c199b67c2a7fc6fe
Evgeniy Tortumashev <mocart1567@gmail.com>
2018-10-19 01:55:41 +0700

    Fix issue 55 DefaultFieldAndOp = true

--- modules/utils/defaultUtils.js
+++ modules/utils/defaultUtils.js
@@ -3,6 +3,7 @@ import map from 'lodash/map';
 import range from 'lodash/range';
 import uuid from './uuid';
 import {getFieldConfig, getFirstField, getFirstOperator, getOperatorConfig} from './configUtils';
+import {_getNewValueForFieldOp} from '../stores/tree'
 
 
 export const defaultField = (config, canGetFirst = true) => {
@@ -35,8 +36,7 @@ export const defaultRuleProperties = (config) => {
     field = defaultField(config);
     operator = defaultOperator(config, field);
   }
-
-  return new Immutable.Map({
+  let current = new Immutable.Map({
     field: field,
     operator: operator,
     value: new Immutable.List(),
@@ -44,6 +44,16 @@ export const defaultRuleProperties = (config) => {
     //used for complex operators like proximity
     operatorOptions: defaultOperatorOptions(config, operator, field),
   });
+  if(field && operator){
+
+    let {newValue, newValueSrc, newValueType} = _getNewValueForFieldOp (config, config, current, field, operator, 'operator');
+
+    current = current
+        .set('value', newValue)
+        .set('valueSrc', newValueSrc)
+        .set('valueType', newValueType);
+  }
+  return current; 
 };
 
 //------------
