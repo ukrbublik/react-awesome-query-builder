7b1e4b006c633ab998dc12743fdfa630e2fd5f7d
Denis Oblogin <ukrbublik@gmail.com>
2018-12-12 21:08:50 +0200

    fixed script for macos

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -5,7 +5,7 @@ mkdir /tmp/react-awesome-query-builder
 mkdir /tmp/react-awesome-query-builder/gh-pages
 cp ./examples/bundle.js ./examples/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
 git checkout gh-pages
-cp /tmp/react-awesome-query-builder/gh-pages/* . -R
+cp -R /tmp/react-awesome-query-builder/gh-pages/* .
 git add ./bundle.js
 git add ./bundle.js.map
 git commit -m "update"
