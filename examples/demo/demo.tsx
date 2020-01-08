d5119de4a82a35c54d8a7b1181760c7d380082b9
Nyiko Zoltan <nyikozoli@gmail.com>
2020-01-08 14:31:51 +0100

    setup the demo

--- examples/demo/demo.tsx
+++ examples/demo/demo.tsx
@@ -2,36 +2,37 @@ import React, {Component} from 'react';
 import {
   Query, Builder, BasicConfig, Utils, 
   //types:
-  ImmutableTree, Config, BuilderProps, JsonTree
+  ImmutableTree, Config, BuilderProps, JsonTree, RuleResultProps
 } from 'react-awesome-query-builder';
 import throttle from 'lodash/throttle';
 import loadedConfig from './config';
 import loadedInitValue from './init_value';
 
 const stringify = JSON.stringify;
-const {queryBuilderFormat, jsonLogicFormat, queryString, mongodbFormat, sqlFormat, getTree, checkTree, loadTree, uuid} = Utils;
+const {queryBuilderFormat, jsonLogicFormat, queryString, mongodbFormat, sqlFormat, getTree, checkTree, loadTree, uuid, getFlatTree } = Utils;
 const preStyle = { backgroundColor: 'darkgrey', margin: '10px', padding: '10px' };
 const preErrorStyle = { backgroundColor: 'lightpink', margin: '10px', padding: '10px' };
 
 const emptyInitValue: JsonTree = {id: uuid(), type: "group"};
 const initValue: JsonTree = loadedInitValue && Object.keys(loadedInitValue).length > 0 ? loadedInitValue as JsonTree : emptyInitValue;
 
-
-
 interface DemoQueryBuilderState {
   tree: ImmutableTree;
   config: Config;
+  ruleResults: RuleResultProps[];
 }
 
 export default class DemoQueryBuilder extends Component<{}, DemoQueryBuilderState> {
     private immutableTree: ImmutableTree;
     private config: Config;
+    private ruleResults: RuleResultProps [];
 
     state = {
       tree: checkTree(loadTree(initValue), loadedConfig),
-      config: loadedConfig
+      config: loadedConfig,
+      ruleResults: []
     };
-
+ 
     render = () => (
       <div>
         <Query
@@ -46,30 +47,52 @@ export default class DemoQueryBuilder extends Component<{}, DemoQueryBuilderStat
       </div>
     )
 
-    renderBuilder = (props: BuilderProps) => (
+    renderBuilder = (props: BuilderProps) => {
+      return (
+      
       <div className="query-builder-container" style={{padding: '10px'}}>
           <div className="query-builder qb-lite">
-              <Builder {...props} />
+              <Builder 
+                {...props} 
+                customData={{ruleResults: this.state.ruleResults}} 
+              />
           </div>
       </div>
     )
+  }
     
     onChange = (immutableTree: ImmutableTree, config: Config) => {
       this.immutableTree = immutableTree;
       this.config = config;
+      this.queryBackend();
       this.updateResult();
-      const jsonTree = getTree(immutableTree); //can be saved to backend
     }
 
     updateResult = throttle(() => {
-      this.setState({tree: this.immutableTree, config: this.config});
+      this.setState({tree: this.immutableTree, config: this.config, ruleResults: this.ruleResults});
     }, 100)
+
+    queryBackend() {
+      const jsonTree = getTree(this.immutableTree); //can be saved to backend
+      const flatTree = getFlatTree(this.immutableTree);
+      this.ruleResults = flatTree.flat.map(x => { return { 
+        ruleId: x, 
+        filtered: Math.floor((Math.random() * 10000)), 
+        total: Math.floor((Math.random() * 100000) + 10000) }}) as RuleResultProps[];
+    }
 
     renderResult = ({tree: immutableTree, config} : {tree: ImmutableTree, config: Config}) => {
       const {logic, data, errors} = jsonLogicFormat(immutableTree, config);
       return (
       <div>
         <br />
+        <div>
+          queryBuilderFormat: 
+            <pre style={preStyle}>
+              {stringify(queryBuilderFormat(immutableTree, config), undefined, 2)}
+            </pre>
+        </div>
+        <hr/>
         <div>
           stringFormat: 
           <pre style={preStyle}>
@@ -122,13 +145,6 @@ export default class DemoQueryBuilder extends Component<{}, DemoQueryBuilderStat
             {stringify(getTree(immutableTree), undefined, 2)}
           </pre>
         </div>
-        {/* <hr/>
-        <div>
-          queryBuilderFormat: 
-            <pre style={preStyle}>
-              {stringify(queryBuilderFormat(immutableTree, config), undefined, 2)}
-            </pre>
-        </div> */}
       </div>
       )
   }
