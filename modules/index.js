b89a654645ab213f3160ccb8679eff7b51c4feb2
Nyiko Zoltan <nyikozoli@gmail.com>
2020-01-08 14:31:29 +0100

    export getFlatTree from Utils

--- modules/index.js
+++ modules/index.js
@@ -6,8 +6,10 @@ import * as Import from './import';
 import * as Widgets from './components/widgets/index';
 import * as Operators from './components/operators';
 import * as BasicUtils from './utils';
-const Utils = {...BasicUtils, ...Export, ...Import};
-export {Widgets, Operators, Utils, Export, Import};
+import { getFlatTree } from './utils/treeUtils';
 
-export {default as BasicConfig} from './config/basic';
+const Utils = { ...BasicUtils, ...Export, ...Import, getFlatTree };
+export { Widgets, Operators, Utils, Export, Import };
+
+export { default as BasicConfig } from './config/basic';
 
