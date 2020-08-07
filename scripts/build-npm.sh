62257b2e57615cb74e80aeec73b6de974f91aeb7
James Coleman <james@jamescoleman.com.au>
2020-08-07 23:46:31 +1000

    Compile scss to css for bundle (#262)
    * Compile scss to css for bundle
    
    * add lib/, whoops
    
    Co-authored-by: Denis Oblogin <ukrbublik@gmail.com>

--- scripts/build-npm.sh
+++ scripts/build-npm.sh
@@ -1,5 +1,6 @@
 rm -rf lib
 babel -d lib ./modules
+node-sass css/ -o lib/css/ --output-style compressed
 cp modules/index.d.ts lib/index.d.ts
 cp modules/config/antd/index.d.ts lib/config/antd/index.d.ts
 cp modules/components/widgets/antd/index.d.ts lib/components/widgets/antd/index.d.ts
