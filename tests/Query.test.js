9b3ef38f467f56103173e6d58e7d2e0eca3319c3
Denis Oblogin <ukrbublik@gmail.com>
2020-09-07 00:46:20 +0300

    tests reduce whitespaces

--- tests/Query.test.js
+++ tests/Query.test.js
@@ -191,8 +191,7 @@ describe("query with subquery and datetime types", () => {
         "$or": [
           {
             "datetime": "2020-05-18 21:50:01"
-          },
-          {
+          }, {
             "date": "2020-05-18",
             "time": "00:50:00"
           }
@@ -201,30 +200,13 @@ describe("query with subquery and datetime types", () => {
       "logic": {
         "or": [
           {
-            "==": [
-              {
-                "var": "datetime"
-              },
-              "2020-05-18T21:50:01.000Z"
-            ]
-          },
-          {
+            "==": [ { "var": "datetime" },  "2020-05-18T21:50:01.000Z" ]
+          }, {
             "and": [
               {
-                "==": [
-                  {
-                    "var": "date"
-                  },
-                  "2020-05-18T00:00:00.000Z"
-                ]
-              },
-              {
-                "==": [
-                  {
-                    "var": "time"
-                  },
-                  3000
-                ]
+                "==": [ { "var": "date" },  "2020-05-18T00:00:00.000Z" ]
+              }, {
+                "==": [ { "var": "time" },  3000 ]
               }
             ]
           }
@@ -254,37 +236,18 @@ describe("query with select", () => {
       "sql": "(color = 'yellow' AND multicolor = 'yellow,green')",
       "mongo": {
         "color": "yellow",
-        "multicolor": [
-          "yellow",
-          "green"
-        ]
+        "multicolor": [ "yellow", "green" ]
       },
       "logic": {
         "and": [
           {
-            "==": [
-              {
-                "var": "color"
-              },
-              "yellow"
-            ]
-          },
-          {
+            "==": [ { "var": "color" },  "yellow" ]
+          }, {
             "all": [
-              {
-                "var": "multicolor"
-              },
-              {
-                "in": [
-                  {
-                    "var": ""
-                  },
-                  [
-                    "yellow",
-                    "green"
-                  ]
-                ]
-              }
+              { "var": "multicolor" },
+              { "in": [
+                { "var": "" },  [ "yellow", "green" ]
+              ] }
             ]
           }
         ]
@@ -328,40 +291,18 @@ describe("query with !struct", () => {
           {
             "some": [
               [{ "var": "results" }],
-              {
-                "and": [
-                  {
-                    "==": [
-                      {
-                        "var": "slider"
-                      },
-                      22
-                    ]
-                  },
-                  {
-                    "==": [
-                      {
-                        "var": "stock"
-                      },
-                      true
-                    ]
-                  }
-                ]
-              }
+              { "and": [
+                {
+                  "==": [ { "var": "slider" },  22 ]
+                }, {
+                  "==": [ { "var": "stock" },  true ]
+                }
+              ] }
             ]
-          },
-          {
-            "==": [
-              {
-                "var": "user.firstName"
-              },
-              "abc"
-            ]
-          },
-          {
-            "!!": {
-              "var": "user.login"
-            }
+          }, {
+            "==": [ { "var": "user.firstName" },  "abc" ]
+          }, {
+            "!!": { "var": "user.login" }
           }
         ]
       }
@@ -391,22 +332,14 @@ describe("query with field compare", () => {
       "sql": "num = num2",
       "mongo": {
         "$expr": {
-          "$eq": [
-            "$num",
-            "$num2"
-          ]
+          "$eq": [ "$num", "$num2" ]
         }
       },
       "logic": {
         "and": [
           {
             "==": [
-              {
-                "var": "num"
-              },
-              {
-                "var": "num2"
-              }
+              { "var": "num" },  { "var": "num2" }
             ]
           }
         ]
@@ -439,16 +372,9 @@ describe("query with func", () => {
         "and": [
           {
             "==": [
+              { "var": "str" },
               {
-                "var": "str"
-              },
-              {
-                "method": [
-                  {
-                    "var": "str2"
-                  },
-                  "toLowerCase"
-                ]
+                "method": [ { "var": "str2" },  "toLowerCase" ]
               }
             ]
           }
@@ -485,17 +411,9 @@ describe("query with func", () => {
           "$expr": {
             "$eq": [
               "$num",
-              {
-                "$sum": [
-                  {
-                    "$multiply": [
-                      1,
-                      4
-                    ]
-                  },
-                  0
-                ]
-              }
+              { "$sum": [
+                { "$multiply": [ 1, 4 ] },  0
+              ] }
             ]
           }
         },
@@ -503,20 +421,8 @@ describe("query with func", () => {
           "and": [
             {
               "==": [
-                {
-                  "var": "num"
-                },
-                {
-                  "+": [
-                    {
-                      "*": [
-                        1,
-                        4
-                      ]
-                    },
-                    0
-                  ]
-                }
+                { "var": "num" },
+                { "+": [ { "*": [ 1, 4 ] }, 0 ] }
               ]
             }
           ]
@@ -554,110 +460,47 @@ describe("query with ops", () => {
           }
         },
         "color": {
-          "$in": [
-            "yellow"
-          ],
-          "$nin": [
-            "green"
-          ]
+          "$in": [ "yellow" ],
+          "$nin": [ "green" ]
         },
         "multicolor": {
-          "$ne": [
-            "yellow"
-          ]
+          "$ne": [ "yellow" ]
         }
       },
       "logic": {
         "and": [
           {
-            "!=": [
-              {
-                "var": "num"
-              },
-              2
-            ]
-          },
-          {
-            "in": [
-              "abc",
-              {
-                "var": "str"
-              }
-            ]
-          },
-          {
+            "!=": [ { "var": "num" },  2 ]
+          }, {
+            "in": [ "abc",  { "var": "str" } ]
+          }, {
             "!": {
               "in": [
                 "xyz",
-                {
-                  "var": "str"
-                }
+                { "var": "str" }
               ]
             }
-          },
-          {
-            "<=": [
-              1,
-              {
-                "var": "num"
-              },
-              2
-            ]
-          },
-          {
-            "!": {
-              "<=": [
-                3,
-                {
-                  "var": "num"
-                },
-                4
-              ]
-            }
-          },
-          {
-            "!": {
-              "var": "num"
-            }
-          },
-          {
+          }, {
+            "<=": [  1,  { "var": "num" },  2  ]
+          }, {
+            "!": {  "<=": [ 3,  { "var": "num" },  4 ]  }
+          }, {
+            "!": { "var": "num" }
+          }, {
             "in": [
-              {
-                "var": "color"
-              },
-              [
-                "yellow"
-              ]
+              { "var": "color" },  [ "yellow" ]
             ]
-          },
-          {
+          }, {
             "!": {
               "in": [
-                {
-                  "var": "color"
-                },
-                [
-                  "green"
-                ]
+                { "var": "color" },  [ "green" ]
               ]
             }
-          },
-          {
+          }, {
             "!": {
               "all": [
-                {
-                  "var": "multicolor"
-                },
-                {
-                  "in": [
-                    {
-                      "var": ""
-                    },
-                    [
-                      "yellow"
-                    ]
-                  ]
-                }
+                { "var": "multicolor" },
+                { "in": [ { "var": "" },  [ "yellow" ] ] }
               ]
             }
           }
