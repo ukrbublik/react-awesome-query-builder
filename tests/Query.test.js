0270e6742cc8c1aa72a02ac9a46eab0bea27fbbf
Denis Oblogin <ukrbublik@gmail.com>
2020-09-07 20:33:32 +0300

    antd widgets

--- tests/Query.test.js
+++ tests/Query.test.js
@@ -1012,6 +1012,7 @@ describe("widgets", () => {
 });
 
 
+
 describe("antdesign widgets", () => {
   it("change date value", () => {
     with_qb_ant(configs.with_all_types, inits.with_date, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
@@ -1022,6 +1023,37 @@ describe("antdesign widgets", () => {
       expect_jlogic([null,
         { "and": [{ "==": [ { "var": "date" }, "2020-05-05T00:00:00.000Z" ] }] }
       ]);
+    });
+  });
+
+  it("change select value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_select, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("SelectWidget")
+        .instance()
+        .handleChange("green");
+      expect_jlogic([null,
+        { "and": [{  "==": [ { "var": "color" }, "green" ]  }] }
+      ]);
+    });
+  });
+
+  it("change multiselect value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_multiselect, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("MultiSelectWidget")
+        .instance()
+        .handleChange(["orange"]);
+      expect_jlogic([null, {
+        "and": [
+          {
+            "all": [
+              { "var": "multicolor" },
+              { "in": [ { "var": "" }, [ "orange" ] ] }
+            ]
+          }
+        ]
+      }]);
     });
   });
 
@@ -1056,7 +1088,55 @@ describe("antdesign widgets", () => {
     });
   });
 
-  //todo: datetime, time, slider, bool, multiselect, select, range, slider
+  it("change time value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_time, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("TimeWidget")
+        .instance()
+        .handleChange(moment("0001-01-01 10:30"));
+      expect_jlogic([null,
+        { "and": [{ "==": [ { "var": "time" }, 60*60*10+60*30 ] }] }
+      ]);
+    });
+  });
+
+  it("change datetime value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_datetime, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("DateTimeWidget")
+        .instance()
+        .handleChange(moment("2020-05-05 10:30"));
+      expect_jlogic([null,
+        { "and": [{ "==": [ { "var": "datetime" }, "2020-05-05T10:30:00.000Z" ] }] }
+      ]);
+    });
+  });
+
+  it("change slider value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_slider, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("SliderWidget")
+        .instance()
+        .handleChange(12);
+      expect_jlogic([null,
+        { "and": [{ "==": [ { "var": "slider" }, 12 ] }] }
+      ]);
+    });
+  });
+
+  it("change bool value", () => {
+    with_qb_ant(configs.with_all_types, inits.with_bool, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
+      qb
+        .find("BooleanWidget")
+        .instance()
+        .handleChange(false);
+      expect_jlogic([null,
+        { "and": [{ "==": [ { "var": "stock" }, false ] }] }
+      ]);
+    });
+  });
+
+  //todo: range
 });
 
 
