5a9c3707adbf7ac98c24cdc7f74c490482a48772
Denis Oblogin <ukrbublik@gmail.com>
2020-10-08 02:21:26 +0300

    ...

--- modules/components/widgets/material/core/MaterialButton.jsx
+++ modules/components/widgets/material/core/MaterialButton.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import DeleteIcon from '@material-ui/icons/Delete';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 export default ({type, label, onClick, config}) => {
-  const typeToLabel = {
-    "addRuleGroup": "+",
-    "delGroup": "x",
-    "delRule": "x",
+  const typeToOnlyIcon = {
+    "delGroup": <DeleteIcon />,
+    "delRule": <DeleteIcon />,
+    "addRuleGroup": <AddIcon />,
   };
-  const btnLabel = typeToLabel[type] || label;
-  return <button onClick={onClick}>{btnLabel}</button>;
+  const typeToIcon = {
+    "addRule": <AddIcon />,
+    "addGroup": <AddIcon />,
+  };
+  const typeToColor = {
+    "addRule": "default",
+    "addGroup": "primary",
+    "delGroup": "secondary",
+    "delRule": "secondary",
+  };
+  if (typeToOnlyIcon[type])
+    return <IconButton size="small" onClick={onClick} color={typeToColor[type]}>{typeToOnlyIcon[type]}</IconButton>;
+  else
+    return <Button size="small" onClick={onClick} color={typeToColor[type]} startIcon={typeToIcon[type]}>{label}</Button>;
 };
--- modules/components/widgets/material/core/MaterialConjs.jsx
+++ modules/components/widgets/material/core/MaterialConjs.jsx
@@ -1,32 +1,59 @@
 import React from "react";
+import FormControl from '@material-ui/core/FormControl';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
 
 export default ({id, not, setNot, conjunctionOptions, setConjunction, disabled, readonly, config}) => {
+
   const renderOptions = () => 
     Object.keys(conjunctionOptions).map(key => {
       const {id, name, label, checked} = conjunctionOptions[key];
       let postfix = setConjunction.isDummyFn ? "__dummy" : "";
-      return [
-        <input key={id+postfix} type="radio" id={id+postfix} name={name+postfix} checked={checked} value={key} onChange={onChange} disabled={disabled} />
-        ,
-        <label key={id+postfix+"label"} htmlFor={id+postfix}>{label}</label>
-      ];
+      return (
+        <Button 
+          key={id+postfix} 
+          id={id+postfix} 
+          color={checked ? "primary" : "default"} 
+          value={key} 
+          onClick={onClick.bind(null, key)} 
+          disabled={disabled}
+        >
+          {label}
+        </Button>
+      );
     });
   
   const renderNot = () => {
-    return [
-      <input key={id}  type="checkbox" id={id + "__not"} checked={not} disabled={disabled} onChange={onNotChange} />
-      ,
-      <label key={id+"label"}  htmlFor={id + "__not"}>{config.settings.notLabel || "NOT"}</label>
-    ];
+    return (
+      <Button 
+        key={id}
+        id={id + "__not"}
+        color={not ? "secondary" : "default"} 
+        onClick={onNotClick.bind(null, !not)} 
+        disabled={disabled}
+      >
+        {config.settings.notLabel || "NOT"}
+      </Button>
+    );
   };
 
-  const onChange = e => setConjunction(e.target.value);
+  const onClick = value => setConjunction(value);
+  const onNotClick = checked => setNot(checked);
 
-  const onNotChange = e => setNot(e.target.checked);
-
-  return [
-    config.settings.showNot && renderNot(),
-    renderOptions()
-  ];
+  return (
+    <FormControl>
+      <ButtonGroup 
+        disableElevation 
+        variant="contained" 
+        size="small" 
+        disabled={disabled}
+      >
+        {config.settings.showNot && renderNot()}
+        {renderOptions()}
+      </ButtonGroup>
+    </FormControl>
+  );
   
 };
