f99e4a99f44154aefd8fffe30a4134723e4772a2
Megan Taylor <mtaylor@megantaylor.org>
2020-10-10 01:05:44 -0400

    material-ui date widget

--- modules/components/widgets/material/value/MaterialDate.jsx
+++ modules/components/widgets/material/value/MaterialDate.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import moment from "moment";
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import MomentUtils from '@date-io/moment';
+import { DatePicker } from "@material-ui/pickers";
 
 export default (props) => {
-  const {value, setValue, config, valueFormat, readonly} = props;
-
-  const onChange = e => {
-    let value = e.target.value;
-    if (value == "")
-      value = undefined;
-    setValue(value);
-  };
   
+  const {value, setValue, valueFormat, readonly, customProps, dateFormat, placeholder} = props;
+
+  const isValidSingleValue = (value) => {
+    return !value || value && value.isValid();
+  };
+
+  const formatSingleValue = (value) => {
+    return value && value.isValid() ? value.format(valueFormat) : undefined;
+  };
+
+  const handleChange = (value) => {
+    if (isValidSingleValue(value))
+      setValue(formatSingleValue(value));
+  };
+
   return (
-    <input type="date"  value={value || ""}  disabled={readonly} onChange={onChange} />
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <DatePicker
+        readOnly={readonly}
+        key="widget-date"
+        placeholder={placeholder}
+        format={dateFormat}
+        value={value || null}
+        onChange={handleChange}
+        {...customProps}
+      />
+    </MuiPickersUtilsProvider>
   );
 };
