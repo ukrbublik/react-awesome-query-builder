2c46b8883ec6962187e2f48f39c924f1c6f09790
Megan Taylor <mtaylor@megantaylor.org>
2020-10-10 02:17:20 -0400

    delete commented out code

--- modules/components/widgets/material/value/MaterialTime.jsx
+++ modules/components/widgets/material/value/MaterialTime.jsx
@@ -32,19 +32,3 @@ return (
   </MuiPickersUtilsProvider>
 );
 };
-
-
-// export default (props) => {
-//   const {value, setValue, config, valueFormat, use12Hours, readonly} = props;
-
-//   const onChange = e => {
-//     let value = e.target.value;
-//     if (value == "")
-//       value = undefined;
-//     setValue(value);
-//   };
-  
-//   return (
-//     <input type="time"  value={value || ""}  disabled={readonly} onChange={onChange} />
-//   );
-// };
