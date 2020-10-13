2f6f1e7cba3d43d1ae855cbe55e4a23b321973f1
Megan Taylor <mtaylor@megantaylor.org>
2020-10-13 15:25:16 -0400

    partial material-ui slider widget

--- modules/components/widgets/material/value/MaterialSlider.jsx
+++ modules/components/widgets/material/value/MaterialSlider.jsx
@@ -1,18 +1,61 @@
 import React from "react";
+import Slider from '@material-ui/core/Slider';
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
 
 export default (props) => {
-  const {value, setValue, config, readonly, min, max, step, placeholder} = props;
-  const onChange = e => {
-    let val = e.target.value;
-    if (val === "" || val === null)
-      val = undefined;
-    else
-      val = parseInt(val);
-    setValue(val);
+  const {placeholder, customProps, value, setValue, min, max, step, marks, readonly} = props;
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
   };
-  return [
-    <input key={"number"} type="number"  value={value} placeholder={placeholder} disabled={readonly} min={min} max={max} step={step} onChange={onChange} />
-    ,
-    <input key={"range"} type="range"  value={value} disabled={readonly} min={min} max={max} step={step} onChange={onChange} />
-  ];
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value === '' ? '' : Number(event.target.value));
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+    } else if (value > 100) {
+      setValue(100);
+    }
+  };
+
+  return (
+    <FormControl >
+    <div style={{display: "inline-flex"}}>
+    <TextField 
+        type="number"
+        value={value}
+        placeholder={placeholder}
+        InputProps={{
+          readOnly: readonly,
+        }}
+        inputProps={{
+          min: min,
+          max: max,
+          step: step,
+        }}
+        disabled={readonly}
+        onChange={handleInputChange}
+        onBlur={handleBlur}
+        fullWidth
+        {...customProps}
+      />
+    <div style={{marginLeft: "11px", width: customProps.width || "300px"}}>
+    <Slider 
+            value={typeof value === 'number' ? value : 0}
+            onChange={handleSliderChange}
+  disabled={readonly}
+  min={min}
+  max={max}
+  step={step}
+  // marks={marks}
+  {...customProps}
+  />
+  </div>
+  </div>
+  </FormControl>
+  );
 };
