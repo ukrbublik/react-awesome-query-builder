9187a7d724d67bd07164448526a42f077555fa2f
Denis Oblogin <ukrbublik@gmail.com>
2021-06-14 17:17:02 +0300

    lint karma

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,30 +1,30 @@
-const puppeteer = require('puppeteer');
-if (puppeteer.executablePath().indexOf('/home/ukrbublik/') == 0) {
+const puppeteer = require("puppeteer");
+if (puppeteer.executablePath().indexOf("/home/ukrbublik/") == 0) {
   // ignore for my local dev
 } else {
   process.env.CHROME_BIN = puppeteer.executablePath();
 }
-process.env.TZ = 'Etc/UTC';
-process.env.BABEL_ENV = 'test'; // Set the proper environment for babel
+process.env.TZ = "Etc/UTC";
+process.env.BABEL_ENV = "test"; // Set the proper environment for babel
 
 module.exports = function(config) {
   config.set({
-    basePath: '',
-    frameworks: ['mocha', 'chai', 'es6-shim'],
+    basePath: "",
+    frameworks: ["mocha", "chai", "es6-shim"],
     plugins: [
-      'karma-mocha',
-      'karma-chai',
-      'karma-es6-shim',
-      'karma-chrome-launcher',
-      'karma-sourcemap-loader',
-      'karma-webpack',
-      'karma-mocha-reporter',
-      'karma-coverage',
+      "karma-mocha",
+      "karma-chai",
+      "karma-es6-shim",
+      "karma-chrome-launcher",
+      "karma-sourcemap-loader",
+      "karma-webpack",
+      "karma-mocha-reporter",
+      "karma-coverage",
     ],
 
     files: [
       {
-        pattern: 'karma.tests.js',
+        pattern: "karma.tests.js",
         watched: true,
         served: true,
         included: true,
@@ -32,41 +32,41 @@ module.exports = function(config) {
     ],
     exclude: [],
     preprocessors: {
-      'karma.tests.js': ['webpack', 'sourcemap'],
-      '../modules/**/*': ['coverage']
+      "karma.tests.js": ["webpack", "sourcemap"],
+      "../modules/**/*": ["coverage"]
     },
-    webpack: require('./webpack.config'),
+    webpack: require("./webpack.config"),
     webpackMiddleware: {
-      stats: 'errors-only'
+      stats: "errors-only"
     },
 
-    reporters: ['progress', 'coverage'],
+    reporters: ["progress", "coverage"],
 
     coverageReporter: {
-      dir: '../coverage',
+      dir: "../coverage",
       reporters: [
-          { type: 'html', subdir: 'html' },
-          { type: 'text-summary' },
-          { type: 'lcov' }
+        { type: "html", subdir: "html" },
+        { type: "text-summary" },
+        { type: "lcov" }
       ],
       includeAllSources: true,
       instrumenterOptions: {
-          istanbul: { noCompact: true }
+        istanbul: { noCompact: true }
       }
-  },
+    },
 
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ["ChromeHeadlessNoSandbox"],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox"]
       }
     },
     singleRun: true,
     concurrency: 1
-  })
-}
+  });
+};
