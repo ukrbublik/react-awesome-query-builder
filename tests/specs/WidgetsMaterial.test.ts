05d45d4c0eee2f097fc9c792b01200cbb1922651
Denis Oblogin <ukrbublik@gmail.com>
2021-06-20 19:53:20 +0300

    fix lint

--- tests/specs/WidgetsMaterial.test.ts
+++ tests/specs/WidgetsMaterial.test.ts
@@ -10,34 +10,34 @@ describe("material-ui widgets interactions", () => {
     with_qb_material(configs.with_date_and_time, inits.with_date_and_time, "JsonLogic", (qb, onChange, {expect_jlogic}) => {
       // open date picker for '2020-05-18'
       const openPickerBtn = qb.find(".rule--widget--DATE button.MuiIconButton-root");
-      expect(openPickerBtn, 'openPickerBtn').to.have.length(1);
+      expect(openPickerBtn, "openPickerBtn").to.have.length(1);
       openPickerBtn.simulate("click");
 
       // click on 3rd week, 2nd day of week (should be sunday, 10 day for default US locale)
       const dayBtn = document.querySelector<HTMLElement>(
-        ".MuiDialog-root " + 
-        ".MuiPickersCalendar-week:nth-child(3) " + 
-        "> div:nth-child(2) " + 
-        ".MuiPickersDay-day"
+        ".MuiDialog-root" 
+        + " .MuiPickersCalendar-week:nth-child(3)" 
+        + " > div:nth-child(2)" 
+        + " .MuiPickersDay-day"
       );
-      expect(dayBtn, 'dayBtn').to.exist;
-      expect(dayBtn?.innerText, 'dayBtn').to.eq('11');
+      expect(dayBtn, "dayBtn").to.exist;
+      expect(dayBtn?.innerText, "dayBtn").to.eq("11");
       dayBtn?.click();
 
       // click ok
       const okBtn = document.querySelector<HTMLElement>(
-        ".MuiDialog-root " + 
-        ".MuiDialogActions-root " + 
-        ".MuiButton-root:nth-child(2) "
+        ".MuiDialog-root" 
+        + " .MuiDialogActions-root" 
+        + " .MuiButton-root:nth-child(2)"
       );
-      expect(okBtn, 'okBtn').to.exist;
+      expect(okBtn, "okBtn").to.exist;
       okBtn?.click();
 
       // now input should be '2020-05-11'
       const dateInput = qb.find(".rule--widget--DATE input.MuiInput-input");
-      expect(dateInput, 'dateInput').to.have.length(1);
-      const dateInputValue = dateInput.getDOMNode().getAttribute('value');
-      expect(dateInputValue, 'dateInputValue').to.eq('11.05.2020');
+      expect(dateInput, "dateInput").to.have.length(1);
+      const dateInputValue = dateInput.getDOMNode().getAttribute("value");
+      expect(dateInputValue, "dateInputValue").to.eq("11.05.2020");
 
       expect_jlogic([null,
         {
