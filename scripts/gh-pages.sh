4c344e0f61b0c38e5347f318d3b06d2c23e077b1
Denis Oblogin <ukrbublik@gmail.com>
2021-06-22 01:33:33 +0300

    upd travis (#434)
    * upd travis
    
    * .
    
    * .
    
    * .
    
    * .
    
    * .
    
    * Add .circleci/config.yml
    
    * .
    
    * .
    
    * .
    
    * tst
    
    * t
    
    * t
    
    * .
    
    * .
    
    * .
    
    * examples/build
    
    * .

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -2,11 +2,10 @@ npm run build-examples
 rm -rf /tmp/react-awesome-query-builder
 mkdir /tmp/react-awesome-query-builder
 mkdir /tmp/react-awesome-query-builder/gh-pages
-cp ./examples/bundle.js ./examples/bundle.js.map /tmp/react-awesome-query-builder/gh-pages
+cp ./examples/bundle.* /tmp/react-awesome-query-builder/gh-pages/
 git checkout gh-pages
 cp -R /tmp/react-awesome-query-builder/gh-pages/* .
-git add ./bundle.js
-git add ./bundle.js.map
+git add ./bundle.*
 git commit -m "update"
 git checkout master
 
