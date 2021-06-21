3c760167ce28b6feef567448810c67a8434e3c5e
Denis Oblogin <ukrbublik@gmail.com>
2021-06-22 00:23:46 +0300

    .

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -72,7 +72,7 @@ module.exports = function(config) {
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: "ChromeHeadless",
-        flags: ["--no-sandbox"]
+        flags: ["--no-sandbox", "--disable-setuid-sandbox"]
       }
     },
     singleRun: true,
