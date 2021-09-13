ba64ed4436184f8a45b2db9941ba6aaae745e369
madura <madura@sqrmobile.com>
2021-09-14 02:46:31 +0530

    extra safe option

--- modules/components/widgets/vanilla/value/VanillaSelect.jsx
+++ modules/components/widgets/vanilla/value/VanillaSelect.jsx
@@ -1,24 +1,38 @@
 import React, { useMemo, useCallback } from "react";
 // import { mapListValues } from "../../../../utils/stuff";
 import { Form } from "@shoutout-labs/shoutout-themes-enterprise";
-export default ({ listValues, value, setValue, allowCustomValues, readonly }) => {
-  // const renderOptions = () => 
+export default ({
+  listValues=[],
+  value,
+  setValue,
+  allowCustomValues,
+  readonly,
+  ...rest
+}) => {
+  // const renderOptions = () =>
   //   mapListValues(listValues, ({title, value}) => {
   //     return <option key={value} value={value}>{title}</option>;
   //   });
 
-  const onChange = useCallback(e => {
-    setValue(e[0] ? e[0].value : "")
-  },
-    [setValue]);
+  const onChange = useCallback(
+    (e) => {
+      setValue(e[0] ? e[0].value : "");
+    },
+    [setValue]
+  );
 
   const selectedValue = useMemo(() => {
     if (value) {
-      return [listValues.find((item) => item.value === value)];
+      return [
+        listValues.find((item) => item.value === value) || {
+          title: value,
+          value,
+        },
+      ];
     }
     return [];
-  }, [value, listValues])
- 
+  }, [value, listValues]);
+
   return (
     <Form.Select
       labelKey="title"
@@ -29,8 +43,8 @@ export default ({ listValues, value, setValue, allowCustomValues, readonly }) =>
       options={listValues}
       size="sm"
       placeholder="Select"
+      {...rest}
     />
-
   );
   // <select
   //     onChange={onChange}
