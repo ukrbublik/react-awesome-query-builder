10f67414e769d4d2f96f745e020657cd2b1d00a5
madura <madura@sqrmobile.com>
2021-09-14 02:48:21 +0530

    cleanup

--- modules/components/widgets/vanilla/value/VanillaText.jsx
+++ modules/components/widgets/vanilla/value/VanillaText.jsx
@@ -8,48 +8,22 @@ export default (props) => {
     readonly,
     placeholder,
     maxLength,
-    // listValues = [],
-    // asyncListValues = [],
     fieldDefinition,
     ...rest
   } = props;
 
-  // const [optionsList, setOptionsList] = useState([]);
-
-  // const listValues = asyncListValues;
-
+ 
   const selectedValue = useMemo(() => {
-    // const textValue = value || "";
-    // if (listValues.length > 0) {
-    //   return textValue
-    //     ? [listValues.find((item) => item.value === textValue)]
-    //     : [];
-    // }
     return value ? [value] : [];
   }, [value]);
 
   const onChangeValue = useCallback(
-    (e) => {
-      // console.debug("on slect value:", e, e.length > 0 ? e[0].value : undefined);
+    (e) => { 
       setValue(e.length > 0 ? e[0].value : undefined);
-      // let val = e.target.value;
-      // if (val === "") val = undefined; // don't allow empty value
-      // setValue(val);
     },
     [setValue]
   );
 
-  // const onChangeInput = (text) => {
-  //   if (fieldDefinition && fieldDefinition.asyncFetch) {
-  //     setOptionsList(fieldDefinition.asyncFetch(text).values);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (fieldDefinition && fieldDefinition.asyncFetch) {
-  //     setOptionsList(fieldDefinition.asyncFetch("").values);
-  //   }
-  // }, []);
 
   const options = useMemo(() => {
     //This is a hack to pass the values since asyncFetch isn't functioning
@@ -58,12 +32,7 @@ export default (props) => {
     }
     return [];
   }, [fieldDefinition]);
-  // if (listValues.length > 0) {
-
-  // const selectedValue = value
-  //   ? [listValues.find((item) => item.value === value)]
-  //   : [];
-  console.debug("options list:", options);
+ 
   return (
     <Form.Select
       labelKey="title"
@@ -78,32 +47,5 @@ export default (props) => {
       {...rest}
     />
   );
-  // }
-
-  const onChange = (e) => {
-    let val = e.target.value;
-    if (val === "") val = undefined; // don't allow empty value
-    setValue(val);
-  };
-  // const textValue = value || "";
-
-  return (
-    <Form.Control
-      size="sm"
-      type="text"
-      value={selectedValue}
-      placeholder={placeholder}
-      disabled={readonly}
-      onChange={onChange}
-      maxLength={maxLength}
-    />
-    // <input
-    //   type="text"
-    //   value={textValue}
-    //   placeholder={placeholder}
-    //   disabled={readonly}
-    //   onChange={onChange}
-    //   maxLength={maxLength}
-    // />
-  );
+  
 };
