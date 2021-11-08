3acca94db1f2d3f5d58b47c3b3f986d3be466cc5
Denis Oblogin <ukrbublik@gmail.com>
2021-11-08 23:43:31 +0200

    Spread customProps for input and slider

--- modules/components/widgets/vanilla/value/VanillaSlider.jsx
+++ modules/components/widgets/vanilla/value/VanillaSlider.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 export default (props) => {
-  const {value, setValue, config, readonly, min, max, step, placeholder, customProps, } = props;
+  const {value, setValue, config, readonly, min, max, step, placeholder, customProps = {}, } = props;
+  const customInputProps = customProps.input || {};
+  const customSliderProps = customProps.slider || customProps;
   const onChange = e => {
     let val = e.target.value;
     if (val === "" || val === null)
@@ -12,8 +14,8 @@ export default (props) => {
   };
   const numberValue = value == undefined ? "" : value;
   return [
-    <input key={"number"} type="number"  value={numberValue} placeholder={placeholder} disabled={readonly} min={min} max={max} step={step} onChange={onChange} {...customProps} />
+    <input key={"number"} type="number"  value={numberValue} placeholder={placeholder} disabled={readonly} min={min} max={max} step={step} onChange={onChange} {...customInputProps} />
     ,
-    <input key={"range"} type="range"  value={numberValue} disabled={readonly} min={min} max={max} step={step} onChange={onChange} {...customProps} />
+    <input key={"range"} type="range"  value={numberValue} disabled={readonly} min={min} max={max} step={step} onChange={onChange} {...customSliderProps} />
   ];
 };
