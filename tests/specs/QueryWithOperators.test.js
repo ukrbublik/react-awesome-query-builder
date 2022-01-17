8bd749dc483a40199527e688224cf41725582bc2
Denis Oblogin <ukrbublik@gmail.com>
2022-01-17 14:06:45 +0200

    test fix

--- tests/specs/QueryWithOperators.test.js
+++ tests/specs/QueryWithOperators.test.js
@@ -6,8 +6,8 @@ import { export_checks } from "../support/utils";
 describe("query with ops", () => {
   describe("export", () => {
     export_checks(configs.with_all_types, inits.with_ops, "JsonLogic", {
-      "query": "(num != 2 && str Like \"abc\" && str Not Like \"xyz\" && num >= 1 && num <= 2 && !(num >= 3 && num <= 4) && !num && color IN (\"yellow\") && color NOT IN (\"green\") && multicolor != [\"yellow\"])",
-      "queryHuman": "(Number != 2 AND String Like abc AND String Not Like xyz AND Number BETWEEN 1 AND 2 AND NOT (Number BETWEEN 3 AND 4) AND Number IS NULL AND Color IN (Yellow) AND Color NOT IN (Green) AND Colors != [Yellow])",
+      "query": "(num != 2 && str Like \"abc\" && str Not Like \"xyz\" && num >= 1 && num <= 2 && (num < 3 || num > 4) && !num && color IN (\"yellow\") && color NOT IN (\"green\") && multicolor != [\"yellow\"])",
+      "queryHuman": "(Number != 2 AND String Like abc AND String Not Like xyz AND Number BETWEEN 1 AND 2 AND Number NOT BETWEEN 3 AND 4 AND Number IS NULL AND Color IN (Yellow) AND Color NOT IN (Green) AND Colors != [Yellow])",
       "sql": "(num <> 2 AND str LIKE '%abc%' AND str NOT LIKE '%xyz%' AND num BETWEEN 1 AND 2 AND num NOT BETWEEN 3 AND 4 AND num IS NULL AND color IN ('yellow') AND color NOT IN ('green') AND multicolor != 'yellow')",
       "mongo": {
         "num": {
