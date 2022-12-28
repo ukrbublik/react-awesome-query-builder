89bd1b4ddc115512d1b7cf97f59c3c7f70a5f170
ukrbublik <ukrbublik@gmail.com>
2022-12-28 02:18:44 +0200

    port

--- packages/sandbox/vite.config.ts
+++ packages/sandbox/vite.config.ts
@@ -4,6 +4,10 @@ import reactSupport from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
+  server: {
+    host: true,
+    port: 5173,
+  },
   plugins: [
     reactSupport()
   ],
--- packages/sandbox_simple/vite.config.ts
+++ packages/sandbox_simple/vite.config.ts
@@ -4,6 +4,10 @@ import reactSupport from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
+  server: {
+    host: true,
+    port: 5174,
+  },
   plugins: [
     reactSupport()
   ],
