165539ccc3cd36b6a9fa3ea3e849ab24c8883e3a
Jarrad Whitaker <113399675+jwhitaker-swiftnav@users.noreply.github.com>
2023-03-02 14:44:44 +1100

    add test for jsonlogic isnull roundtrip
    manually:
    (cherry picked from commit 59df7d3ed79ab47775c1ea44497ae9e79ade5fda)

--- tests/specs/QueryWithOperators.test.js
+++ tests/specs/QueryWithOperators.test.js
@@ -2,6 +2,7 @@ import * as configs from "../support/configs";
 import * as inits from "../support/inits";
 import { export_checks } from "../support/utils";
 
+import { BasicConfig, Utils } from "react-awesome-query-builder";
 
 describe("query with ops", () => {
   describe("export", () => {
@@ -76,4 +77,15 @@ describe("query with ops", () => {
       }
     });
   });
+
+  describe("round trip", () => {
+    it("should work", () => {
+      const config = configs.with_all_types(BasicConfig)
+      const input = { "and": [{ "==": [{ "var": "color" }, null] }] };
+      const [tree, errs] = Utils._loadFromJsonLogic(input, config, true);
+      expect(errs).to.deep.equal([]);
+      const output = Utils.jsonLogicFormat(tree, config);
+      expect(output.logic).to.deep.equal(input);
+    });
+  })
 });
