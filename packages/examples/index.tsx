6ec9facd3d7611fc866e815050332caa26a93fe4
ukrbublik <ukrbublik@gmail.com>
2023-04-04 12:29:26 +0300

    react 18 + strict

--- packages/examples/index.tsx
+++ packages/examples/index.tsx
@@ -1,6 +1,7 @@
 
 import React, {Component} from "react";
 import ReactDOM from "react-dom";
+import * as ReactDOMClient from 'react-dom/client';
 const Demo = React.lazy(() => import("./demo"));
 const DemoSwitch = React.lazy(() => import("./demo_switch"));
 import {
@@ -10,12 +11,15 @@ import {
 } from "react-router-dom";
 
 const rootElement = window.document.getElementById("root");
+const root = ReactDOMClient.createRoot(rootElement);
 
-ReactDOM.render((
+root.render((
+  <React.StrictMode>
   <HashRouter>
     <Routes>
       <Route path="/switch" element={<React.Suspense fallback={<>...</>}><DemoSwitch /></React.Suspense>} />
       <Route path="*" element={<React.Suspense fallback={<>...</>}><Demo /></React.Suspense>} />
     </Routes>
   </HashRouter>
-), rootElement);
+  </React.StrictMode>
+));
