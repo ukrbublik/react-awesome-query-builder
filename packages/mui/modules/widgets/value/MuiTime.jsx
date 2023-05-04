d8a27fe787141014c277a9b3b6f5d32a5641c3f6
ukrbublik <ukrbublik@gmail.com>
2023-05-04 17:49:46 +0300

    ?

--- packages/mui/modules/widgets/value/MuiTime.jsx
+++ packages/mui/modules/widgets/value/MuiTime.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
+//import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import FormControl from "@mui/material/FormControl";
 import TextField from "@mui/material/TextField";
 import { Utils } from "@react-awesome-query-builder/ui";
+import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
+import { DesktopTimePicker } from "@mui/x-date-pickers/DesktopTimePicker";
 const { moment } = Utils;
 
 export default (props) => {
-  const {value, setValue, use12Hours, readonly, placeholder, timeFormat, valueFormat, customProps} = props;
+  const {value, setValue, use12Hours, readonly, placeholder, timeFormat, valueFormat, customProps, useKeyboard} = props;
 
   const formatSingleValue = (value) => {
     return value && value.isValid() ? value.format(valueFormat) : undefined;
@@ -28,6 +30,7 @@ export default (props) => {
 
   // const desktopModeMediaQuery = "@media (pointer: fine) or (pointer: none)";
   const desktopModeMediaQuery = "@media not (pointer: coarse)";
+  const TimePicker = useKeyboard ? DesktopTimePicker : MobileTimePicker;
 
   return (
     <FormControl>
