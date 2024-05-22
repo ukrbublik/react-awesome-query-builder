e4dab510a9d8a3f8628a7376ce934cd49cb3dfee
ukrbublik <ukrbublik@gmail.com>
2024-05-23 02:23:33 +0300

    fix lint

--- packages/tests/support/utils.tsx
+++ packages/tests/support/utils.tsx
@@ -687,7 +687,9 @@ const expect_jlogic_before_and_after = (config: Config, tree: ImmutableTree, onC
 };
 
 export const expect_objects_equal = (act: any, exp: any, actLabel?: string, expLabel?: string) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const expSafe = exp ? JSON.parse(JSON.stringify(exp)) : exp;
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const actSafe = act ? JSON.parse(JSON.stringify(act)) : act;
   expect(actSafe).to.eql(expSafe);
 };
