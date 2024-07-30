6218aac88b4030bf850064bb9bf8455c40013d71
ukrbublik <ukrbublik@gmail.com>
2024-07-30 20:54:30 +0300

    remove rsync

--- packages/antd/scripts/build-npm.sh
+++ packages/antd/scripts/build-npm.sh
@@ -3,13 +3,18 @@ rm -rf ./cjs
 rm -rf ./esm
 
 babel --extensions ".tsx,.jsx,.ts,.js" -d ./cjs ./modules
+find cjs/ -type f -name "*.d.js" | xargs -I{} rm {}
 #find ./cjs -name "*.js" -exec sed -i.bak "s+antd/es/+antd/lib/+g" {} +
 #rm ./cjs/**/*.bak
 node ./scripts/fix-antd.js
-ESM=1 babel --extensions ".tsx,.jsx,.ts,.js" -d ./esm ./modules
 
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+ESM=1 babel --extensions ".tsx,.jsx,.ts,.js" -d ./esm ./modules
+find esm/ -type f -name "*.d.js" | xargs -I{} rm {}
+
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} cjs/{}
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} esm/{}
 
 rm -rf ./css
 sass -I node_modules -I ../../node_modules styles/:css/ --no-source-map --style=expanded
--- packages/core/scripts/build-npm.sh
+++ packages/core/scripts/build-npm.sh
@@ -5,5 +5,7 @@ rm -rf ./esm
 babel -d ./cjs ./modules
 ESM=1 babel -d ./esm ./modules
 
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} cjs/{}
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} esm/{}
