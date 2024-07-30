128adb82bbe737c87b8b533dae86023515a8db31
ukrbublik <ukrbublik@gmail.com>
2024-07-30 20:54:30 +0300

    remove rsync

--- packages/mui/scripts/build-npm.sh
+++ packages/mui/scripts/build-npm.sh
@@ -5,8 +5,10 @@ rm -rf ./esm
 babel -d ./cjs ./modules
 ESM=1 babel -d ./esm ./modules
 
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
-rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./cjs/
+# rsync -ma --include '*/' --include '*.d.ts' --exclude '*' ./modules/ ./esm/
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} cjs/{}
+find modules/ -type f -name "*.d.ts" | cut -d'/' -f2- | xargs -I{} cp modules/{} esm/{}
 
 rm -rf ./css
 sass -I node_modules -I ../../node_modules styles/:css/ --no-source-map --style=expanded
