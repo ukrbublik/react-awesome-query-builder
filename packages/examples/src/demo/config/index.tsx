e656e0cf356475154fbcb0c2976d3053b4a994fd
Denys Oblohin <ukrbublik@gmail.com>
2025-01-15 20:52:10 +0200

    tokenSeparators in example

--- packages/examples/src/demo/config/index.tsx
+++ packages/examples/src/demo/config/index.tsx
@@ -584,6 +584,11 @@ export default (skin: string) => {
           orange: "Orange"
         },
         allowCustomValues: true,
+      },
+      mainWidgetProps: {
+        customProps: {
+          tokenSeparators: [","]
+        }
       }
     },
     selecttree: {
