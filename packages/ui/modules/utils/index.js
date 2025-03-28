3315f735c942e635df86dc7406fd50bdd6dd4803
Denys Oblohin <ukrbublik@gmail.com>
2025-03-28 16:28:09 +0200

    add chroma, color utils, level bg vars

--- packages/ui/modules/utils/index.js
+++ packages/ui/modules/utils/index.js
@@ -1,4 +1,6 @@
 import { Utils } from "@react-awesome-query-builder/core";
 import * as ReactUtils from "./reactUtils";
 import { DragIcon } from "../components/utils";
-export default { ...Utils, ReactUtils, DragIcon };
+import * as ColorUtils from "./colorUtils";
+
+export default { ...Utils, ReactUtils, DragIcon, ColorUtils };
