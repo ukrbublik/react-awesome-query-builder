d96d4ec9d8a7f462d3fa78704b8efbe156ee09ed
Denys Oblohin <ukrbublik@gmail.com>
2025-04-21 22:40:24 +0300

    fix for antd

--- packages/ui/modules/utils/numberFormat.js
+++ packages/ui/modules/utils/numberFormat.js
@@ -1,6 +1,6 @@
-import { NumericFormat, numericFormatter as _numericFormatter } from "react-number-format";
+import { NumericFormat, numericFormatter as _numericFormatter, removeNumericFormat, useNumericFormat } from "react-number-format";
 
-export const getNumberFormatProps = (props, excludePropsNames = []) => {
+const getNumberFormatProps = (props, excludePropsNames = []) => {
   const propNames = [
     // Numeric format
     "prefix",
@@ -26,43 +26,153 @@ export const getNumberFormatProps = (props, excludePropsNames = []) => {
   return existingProps;
 };
 
-export const numericFormatter = (val, numericFormatProps) => {
+const numericFormatter = (val, numericFormatProps) => {
   if (val == undefined) {
     return "";
   }
   return _numericFormatter(""+val, numericFormatProps);
 };
 
-export const numericParser = (str, numericFormatProps) => {
-  const decimalSeparator = numericFormatProps?.decimalSeparator ?? ".";
+// Copied from https://github.com/s-yadav/react-number-format/blob/master/src/utils.tsx#L309
+function getDefaultChangeMeta(value) {
+  return {
+    from: {
+      start: 0,
+      end: 0,
+    },
+    to: {
+      start: 0,
+      end: value.length,
+    },
+    lastValue: "",
+  };
+}
+
+// Copied from https://github.com/s-yadav/react-number-format/blob/master/src/utils.tsx#L257
+const findChangeRange = (prevValue, newValue) => {
+  let i = 0,
+    j = 0;
+  const prevLength = prevValue.length;
+  const newLength = newValue.length;
+  while (prevValue[i] === newValue[i] && i < prevLength) i++;
+
+  //check what has been changed from last
+  while (
+    prevValue[prevLength - 1 - j] === newValue[newLength - 1 - j] &&
+    newLength - j > i &&
+    prevLength - j > i
+  ) {
+    j++;
+  }
+
+  return {
+    from: { start: i, end: prevLength - j },
+    to: { start: i, end: newLength - j },
+  };
+};
+
+// Helps with generating correct `changeMeta` for proper work of `removeNumericFormat()`
+const getChangeMeta = (newValue, prevValue = undefined) => {
+  if (prevValue != undefined) {
+    return { ...findChangeRange(prevValue, newValue), lastValue: newValue };
+  } else {
+    return { ...getDefaultChangeMeta(newValue), lastValue: newValue ?? "" };
+  }
+};
+
+// Apply `decimalScale`
+const limitToScale = (num, numericFormatProps) => {
   const decimalScale = numericFormatProps?.decimalScale;
+  const isNegative = num < 0;
+  // if num == -3.141
+  const positiveWholeNumberPart = Math.abs(isNegative ? Math.ceil(num) : Math.floor(num)); // 3
+  let decimalStr = (""+num).split(".")[1] ?? ""; // "141"
+  let decimalPart = decimalStr != undefined ? parseFloat("0." + decimalStr) : undefined;
+  if (decimalScale && decimalStr.length > decimalScale) {
+    decimalStr = decimalStr.substring(0, decimalScale); // "14"
+    decimalPart = parseFloat("0." + decimalStr); // 0.14
+  }
+  const res = (isNegative ? -1 : 1) * (positiveWholeNumberPart + decimalPart); // -3.14
+  return res;
+};
 
-  let cleanStr = str.replace(new RegExp(`[^0-9\\-\\${decimalSeparator}\\.]`, "g"), "");
-  const isNegative = cleanStr?.startsWith("-");
-  cleanStr = (isNegative ? "-" : "") + cleanStr.replace(/-+/g, "");
-  cleanStr = cleanStr.replace(new RegExp(`\\${decimalSeparator}+|\\.+`, "g"), ".");
-  let parts = cleanStr.split(".");
-  if (parts.length > 2) {
-    parts = [parts.slice(0, -1).join(""), parts.slice(-1)];
-    cleanStr = parts.join(".");
+const startsWithZero = (num) => {
+  const isNegative = num < 0;
+  const positiveWholeNumberPart = Math.abs(isNegative ? Math.ceil(num) : Math.floor(num));
+  return positiveWholeNumberPart === 0;
+};
+
+const getExtraLeadingZeros = (str, num) => {
+  const leadingZeros = str.match(/^-?(0+).+/)?.[1]?.length;
+  const extraLeadingZeros = leadingZeros ? (leadingZeros - (startsWithZero(num) ? 1 : 0)) : undefined;
+  return extraLeadingZeros;
+};
+
+const getTrailingZeros = (str) => {
+  const parts = str.split(".");
+  if (parts[1]?.match(/^0+$/)) {
+    return parts[1].length;
+  }
+  return undefined;
+};
+
+const fixDecimal = (origStr, numericFormatProps, lastStrValue = undefined) => {
+  const decimalSeparator = numericFormatProps?.decimalSeparator ?? ".";
+  let str = origStr;
+  if (lastStrValue?.endsWith(".") && origStr.indexOf(lastStrValue) === 0) {
+    // If decimal separator is "," but user types "." and numbers - treat this as decimal part
+    str = lastStrValue.substring(0, lastStrValue.length - 1) + decimalSeparator + origStr.substring(lastStrValue.length);
+  }
+  return str;
+};
+
+const numericParser = (origStr, numericFormatProps, lastStrValue = undefined) => {
+  const str = fixDecimal(origStr, numericFormatProps, lastStrValue);
+  const changeMeta = getChangeMeta(str, lastStrValue);
+  const cleanStr = removeNumericFormat(str, changeMeta, numericFormatProps);
+ 
+  let num = parseFloat(cleanStr);
+  num = !isNaN(num) && isFinite(num) ? num : undefined;
+  if (num != undefined) {
+    num = limitToScale(num, numericFormatProps);
   }
 
-  const num = parseFloat(cleanStr);
-  let res = !isNaN(num) && isFinite(num) ? num : undefined;
-  if (res != undefined) {
-    // if num == -3.141
-    const positiveWholeNumberPart = Math.abs(isNegative ? Math.ceil(num) : Math.floor(num)); // 3
-    let decimalStr = (""+res).split(".")[1] ?? ""; // "141"
-    let decimalPart = decimalStr != undefined ? parseFloat("0." + decimalStr) : undefined;
-    if (decimalScale && decimalStr.length > decimalScale) {
-      decimalStr = decimalStr.substring(0, decimalScale); // "14"
-      decimalPart = parseFloat("0." + decimalStr); // 0.14
-    }
-    res = (isNegative ? -1 : 1) * (positiveWholeNumberPart + decimalPart); // -3.14
-  }
+  const { max, min, allowLeadingZeros } = numericFormatProps;
+  const isValid = num != undefined && (max == undefined || num <= max) && (min == undefined || num >= min);
+  const extraLeadingZeros = allowLeadingZeros ? getExtraLeadingZeros(cleanStr, num) : undefined;
+  const trailingZeros = getTrailingZeros(cleanStr);
+
+  const res = {
+    str: origStr,
+    num,
+    extraLeadingZeros,
+    trailingZeros,
+    isValid,
+    // extra
+    cleanStr,
+  };
+  return res;
+};
+
+const addLeadingZeros = (str, extraLeadingZeros) => {
+  const isNegative = str.startsWith("-");
+  const positiveStr = isNegative ? str.replace(/^[-]+/, "") : str;
+  const res = (isNegative ? "-" : "") + "".padStart(extraLeadingZeros, "0") + positiveStr;
+  return res;
+};
+
+const addTrailingZeros = (str, trailingZeros) => {
+  const hasDot = str.endsWith(".");
+  const res = str + (hasDot ? "" : ".") + "".padStart(trailingZeros, "0");
   return res;
 };
 
 export {
   NumericFormat,
+  useNumericFormat,
+  numericParser,
+  numericFormatter,
+  getNumberFormatProps,
+  addLeadingZeros,
+  addTrailingZeros,
 };
