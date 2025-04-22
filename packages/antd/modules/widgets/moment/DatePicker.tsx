be5245495fedd4993cdc14a764291d1b49168ac7
ben-swift <87025129+ben-swift@users.noreply.github.com>
2025-04-21 22:35:21 -0400

    support ant 4.x DatePicker

--- packages/antd/modules/widgets/moment/DatePicker.tsx
+++ packages/antd/modules/widgets/moment/DatePicker.tsx
@@ -2,6 +2,13 @@ import type { Moment } from "@react-awesome-query-builder/ui";
 import { DatePicker } from "antd";
 import momentGenerateConfig from "./config";
 
-const MomentDatePicker = DatePicker.generatePicker<Moment>(momentGenerateConfig);
+const MomentDatePicker = (() => {
+  if (typeof DatePicker.generatePicker === "function") {
+    return DatePicker.generatePicker<Moment>(momentGenerateConfig);
+  } else {
+    // support ant 4.x
+    return DatePicker;
+  }
+})();
 
 export default MomentDatePicker;
