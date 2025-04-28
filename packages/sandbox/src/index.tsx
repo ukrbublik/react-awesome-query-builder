7d97d652156e73f76551f07a14507076be9261b0
Denys Oblohin <ukrbublik@gmail.com>
2025-04-28 16:02:01 +0300

    Update dependencies (#1246)
    * Add @babel/runtime to deps
    
    * Update @babel/*
    
    * Upd puppeteer to upd tar-fs
    
    * Upd vite to upd esbuild
    
    * Add @ant-design/v5-patch-for-react-19 for sandbox
    
    * upd lock
    
    * lint fix
    
    * vuln fix
    
    * useUnstableSetRender = false
    
    * .
    
    * lint fix

--- packages/sandbox/src/index.tsx
+++ packages/sandbox/src/index.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot, Root } from "react-dom/client";
+// https://ant.design/docs/react/v5-for-19
+import "@ant-design/v5-patch-for-react-19";
+import { unstableSetRender } from "antd";
 import Demo from "./demo/demo";
 
+const useUnstableSetRender = false;
+
 console.log("React version:", React.version);
+
+// https://ant.design/docs/react/v5-for-19
+if (useUnstableSetRender) {
+  console.log("Using unstableSetRender for antd + react 19, see https://ant.design/docs/react/v5-for-19");
+  unstableSetRender((node, container) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    (container as any)._reactRoot ||= createRoot(container);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const root = (container as any)._reactRoot as Root;
+    root.render(node);
+    return async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      root.unmount();
+    };
+  });
+}
 
 interface AppState {
   skin: string;
@@ -24,6 +45,15 @@ const App: React.FC = () => {
         <option value={"mui"}>mui</option>
         <option value={"antd"}>antd</option>
       </select>
+      {state.skin === "antd" && (
+        <div>
+          Warning: antd v5 is not fully compatible with React 19.
+          <br />
+          You can notice issues when trying to delete rule/group.
+          <br />
+          <a href="https://ant.design/docs/react/v5-for-19" target="_blank" rel="noopener noreferrer">Read more</a>
+        </div>
+      )}
       <Demo skin={state.skin} />
     </div>
   );
